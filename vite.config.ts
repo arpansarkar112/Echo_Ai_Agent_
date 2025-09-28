cb34ca2c4e7fece7574250b9279335c794e42fd2
arpansarkar112 <arpansarkar112@gmail.com>
2025-09-28 22:37:48 +0300

    chore: force rebuild by editing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+// Force rebuild to break cache
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
