8b4622d89f1825bd57ab5489acf3ddd76c1c1697
arpansarkar112 <arpansarkar112@gmail.com>
2025-09-28 23:45:36 +0300

    build: add dummy build config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // Force rebuild to break cache
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    // This is a dummy object to force a cache bust on Vercel.
+  },
   server: {
     host: "::",
     port: 8080,
